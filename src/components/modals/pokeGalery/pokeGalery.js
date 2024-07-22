347ac971933f67d5e0735bfcd8492d6ba0ecb46b
luisamtag <cielitoazul.1027@gmail.com>
2024-07-22 18:48:25 -0500

    Modification-pokeGalery

--- src/components/modals/pokeGalery/pokeGalery.js
+++ src/components/modals/pokeGalery/pokeGalery.js
@@ -1,7 +1,19 @@
 import './pokeGalery.scss';
+import { getData } from '../../../services/axios-service';
+
+const URL_API = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
 
 export function createFooter() {
     const footer = document.createElement('footer');
     footer.innerHTML = `<p>&copy; 2024 Mi Sitio Web</p>`;
     return footer;
+  }
+
+  export async function getPokemon() {
+    try {
+      const dataPokemon = await getData();
+      return dataPokemon;
+    } catch (error) {
+  
+    }
   }
