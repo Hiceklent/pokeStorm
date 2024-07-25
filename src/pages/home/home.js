3e2ac6846b7a606018f72ebf49b6f563b1074b34
luisamtag <cielitoazul.1027@gmail.com>
2024-07-25 13:34:16 -0500

    Add = footer routes

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,18 @@
 import './home.scss';
 import { createHeader } from '../../components/common/header/header';
+import { createFooter } from '../../components/common/footer/footer';
 
 export function createHome() {
     const main = document.createElement('main');
-
     const header = createHeader();
+    const footer = createFooter();
 
     main.innerHTML = /*html*/ `
     <h2>Aqui va el main</h2>
     `;
 
+
     document.body.prepend(header);
     document.body.appendChild(main);
+    document.body.append(footer);
 }
