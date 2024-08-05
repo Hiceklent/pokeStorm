60455fdbd3b83ed67879ecb09f3d8ad2bec26503
Holguer Pinzon <93726097+Hiceklent@users.noreply.github.com>
2024-08-05 04:32:41 -0500

    update - wp

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     filename: "[name].[contenthash].js",
     chunkFilename: '[name].js',
     clean: true,
